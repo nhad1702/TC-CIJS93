351a90f8098cd3406c1219e91cc5d22e2727c8bf
nhad1702 <anhducnguyenhuu2@gmail.com>
2024-03-06 22:04:08 +0700

    lesson 04

---
+++ ls04/src/TodoApp/task/task.jsx
@@ -0,0 +1,11 @@
+function task(props) {
+    return (
+        <>
+            <input type="checkbox" name="" id="" />
+            <p>Task name</p>
+            <button>Remove</button>
+        </>
+    )
+}
+
+export default task;
