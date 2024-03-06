aee68960a80b4b02330e2b6b8d6d8488880bd5f3
nhad1702 <anhducnguyenhuu2@gmail.com>
2024-03-06 19:03:00 +0700

    first commit

---
+++ ls02/script.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+function twoSum(a, b) {
+    return a + b;
+}
+
+console.log(twoSum(2, 5));
+
+const date = new Date();
+year = date.getFullYear;
+month = date.getMonth;
+day = date.getDay;
+hour = date.getHours;
+miinute = date.getMinutes;
+second = date.getSeconds;
+
+// setInterval( () => {
+//     console.log(year + ", ")
+// }, 10000)
+
